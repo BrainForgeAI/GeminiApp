7296eef64f16904f5cb8a72b842393c7a21e94be
Abhinav Ramesh <dbossgames20@gmail.com>
2024-07-26 18:39:31 -0400

    psf and txt file upload is same process, so make into single function

--- Question Generator/load_syllabus.py
+++ Question Generator/load_syllabus.py
@@ -6,33 +6,30 @@ from prompts import summarize_prompt
 if TYPE_CHECKING:
     from google.generativeai import GenerativeModel
 
-def load_syllabus_pdf(gemini_model: 'GenerativeModel', path_to_pdf: str | pathlib.Path
+def load_syllabus_pdf(gemini_model: 'GenerativeModel', path_to_file: str | pathlib.Path
                       | os.PathLike = None, file_name: str = None):
     """
     Loads a PDF of a course syllabus and extracts a summary from it.
     
     :param gemini_model:
         Reference to a gemini model.
-    :param path_to_pdf:
-        Path to pdf, preferabally as a raw string.
+    :param path_to_file:
+        Path to syllabus file, preferabally as a raw string.
     :param file_name:
         If you want to use a previously uploaded file, pass the name of the file, files/{name}.
     """
-    if path_to_pdf and file_name:
-        raise ValueError("Both 'path_to_pdf' and 'file_name' cannot both be passed as argummets")
+    if path_to_file and file_name:
+        raise ValueError("Both 'path_to_file' and 'file_name' cannot both be passed as argummets")
     elif file_name:
         file_ref = genai.get_file(name=file_name)
     else:
-        file_ref = genai.upload_file(path_to_pdf)
+        file_ref = genai.upload_file(path_to_file)
     
     response = gemini_model.generate_content(
                 [file_ref, summarize_prompt])
     
     return response, response.text
 
-# TODO: Load txt file as well.
-def load_syllabus_txt():
-    raise NotImplementedError
 
 # TODO: Method to get file path from user (E.g. from the game).
 def get_file_path():
