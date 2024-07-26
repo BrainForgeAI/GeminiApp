74449e9197ef1ee3cfd7c823c9142840f65bdfb2
sameersul <sameerjuin@gmail.com>
2024-07-25 22:23:16 -0400

    made it look more like the figma

--- Landing_page/geminiapp_website/src/components/how_it_works.jsx
+++ Landing_page/geminiapp_website/src/components/how_it_works.jsx
@@ -5,21 +5,13 @@ const HowItWorks = () => {
     <div className="container">
       <section className="hero">
         <h1>PLAY <span>SMART.</span> GROW <span>SMARTER.</span></h1>
-        <p className="quick-intro">EduQuest harnesses Google’s Gemini AI Model to revolutionize learning. Our adaptive platform creates personalized game-like experiences, 
+        <p className="quick-intro">EduQuest harnesses Google's Gemini AI Model to revolutionize learning. Our adaptive platform creates personalized game-like experiences, 
           tailoring challenges to each user's pace and style. We make education engaging and accessible for everyone. The joy of learning is something everyone deserves to experience</p>
       </section>
 
       <section>
-      <button className="download-button">DOWNLOAD NOW</button> 
-        <div className="card-grid">
-          <div className="card">
-
-            {/* <div className="card-content"> */}
-              {/* <h3 className="card-title">GAME BOX</h3> */}
-            {/* </div> */}
-          </div>
-          {/* Add more cards as needed */}
-        </div>
+          <div className="video-box"></div>
+          <button className="download-button">LEARN MORE</button>   
       </section>
     </div>
   );
