b43fb773a3ba8e5425a622cd21ab0df363850ad2
Devansh <d5nagpal@uwaterloo.ca>
2024-07-26 20:05:44 -0400

    embedded youtube video into home page

--- Landing_page/geminiapp_website/src/components/home.jsx
+++ Landing_page/geminiapp_website/src/components/home.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
+import backgroundImage from '../Assets/background.png'
 
 const Homepage = () => {
   return (
-    <div className="container">
-      <section className="hero">
-        <h1>PLAY <span>SMART.</span> GROW <span>SMARTER.</span></h1>
-        <p className="quick-intro">EduQuest harnesses Google's Gemini AI Model to revolutionize learning. Our adaptive platform creates personalized game-like experiences, 
-          tailoring challenges to each user's pace and style. We make education engaging and accessible for everyone. The joy of learning is something everyone deserves to experience</p>
-      </section>
+    <div className="full-page-background" style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <div className="container">
+        <section className="hero">
+          <h1>PLAY <span>SMART.</span> GROW <span>SMARTER.</span></h1>
+          <p className="quick-intro">EduQuest harnesses Google's Gemini AI Model to revolutionize learning. Our adaptive platform creates personalized game-like experiences, 
+            tailoring challenges to each user's pace and style. We make education engaging and accessible for everyone. The joy of learning is something everyone deserves to experience</p>
+        </section>
 
-      <section>
-          <div className="video-box"></div>
-          <button className="download-button">LEARN MORE</button>   
-      </section>
+        <section>
+            <div className="video-box">
+            <iframe
+            width="560" 
+            height="315"
+            src="https://www.youtube.com/embed/PLUhJmqHd1s" 
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen>
+          </iframe>
+            </div>
+            <button className="download-button">LEARN MORE</button>   
+        </section>
+      </div>
     </div>
   );
 };
