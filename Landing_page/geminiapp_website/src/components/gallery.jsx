20daa1e441c9361cf57f48b6ad83c25684918dd0
vchahal24 <vikramchahal24@gmail.com>
2024-08-12 13:40:57 -0400

    Added background to gallery

--- Landing_page/geminiapp_website/src/components/gallery.jsx
+++ Landing_page/geminiapp_website/src/components/gallery.jsx
@@ -1,7 +1,8 @@
-import React, {useState} from "react";
+import React, { useState, useEffect} from "react";
 import "../styles/gallery.css";
-import Masonry, {ResponsiveMasonry} from "react-responsive-masonry";
+import Masonry from "react-responsive-masonry";
 
+// Import images
 import gameui from "../Assets/Gallery/gameui.png";
 import dialogue from "../Assets/Gallery/dialogue.png";
 import characterModel from "../Assets/Gallery/character-model.png";
@@ -19,59 +20,74 @@ const images = [
 ];
 
 const Gallery = () => {
-  const [data, setData] = useState({img: '', i: 0})
+  const [data, setData] = useState({ img: '', i: 0 });
+  
+  const viewPhoto = (img, i) => {
+    setData({ img, i });
+  };
 
-  const viewPhoto = (img, i)=>{
-      setData({img, i})
-  }
-
-  const buttonCommand = (command) =>{
+  const buttonCommand = (command) => {
     let i = data.i;
-    if (command === 'next-img'){
-      setData({img: images[i + 1], i: i + 1});
+    if (command === 'next-img' && i < images.length - 1) {
+      setData({ img: images[i + 1], i: i + 1 });
     }
-    if (command === 'prev-img'){
-      setData({img: images[i - 1], i: i - 1});
+    if (command === 'prev-img' && i > 0) {
+      setData({ img: images[i - 1], i: i - 1 });
     }
-    if (!command){
-      setData({img: '', i: 0});
+    if (!command) {
+      setData({ img: '', i: 0 });
     }
-  }
+  };
 
   return (
-    <>
-      {data.img &&
-        <div style={{
+    <div className="background-container">
+      {data.img && (
+        <div
+          style={{
             width: '100%',
             height: '100vh',
             background: 'black',
             position: 'fixed',
-            display: 'center',
+            display: 'flex',
             justifyContent: 'center',
             alignItems: 'center',
             overflow: 'hidden',
-        }}>
-          <button className="gallery-btn" onClick={() => buttonCommand()} style={{position: 'absolute', top: '10px', right: '10px'}}>
+          }}
+        >
+          <button
+            className="gallery-btn"
+            onClick={() => buttonCommand()}
+            style={{ position: 'absolute', top: '10px', right: '10px' }}
+          >
             Exit
           </button>
-          <button className="gallery-btn" onClick={()=> buttonCommand('prev-img')}>Previous</button>
-          <img src = {data.img} style = {{width: 'auto', maxWidth: '85%', maxHeight: '85%'}} />
-          <button className="gallery-btn" onClick={() => buttonCommand('next-img')}>Next</button>
+          <button className="gallery-btn" onClick={() => buttonCommand('prev-img')}>
+            Previous
+          </button>
+          <img
+            src={data.img}
+            style={{ width: 'auto', maxWidth: '85%', maxHeight: '85%' }}
+            alt="Selected"
+          />
+          <button className="gallery-btn" onClick={() => buttonCommand('next-img')}>
+            Next
+          </button>
         </div>
-      }
-      <div style={{padding: '12px'}}>
+      )}
+      <div style={{ padding: '12px' }}>
         <Masonry columnsCount={2} gutter="15px">
           {images.map((image, i) => (
             <img
               key={i}
               src={image}
-              style={{width: "100%", display: "block", cursor: 'pointer'}}
-              onClick={()=>viewPhoto(image, i)}
+              style={{ width: '100%', display: 'block', cursor: 'pointer' }}
+              onClick={() => viewPhoto(image, i)}
+              alt={`Gallery image ${i}`}
             />
           ))}
         </Masonry>
       </div>
-    </>
+    </div>
   );
 };
 
