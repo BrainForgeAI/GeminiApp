8127b62e0c053f99144b86b831d924e3ade72633
Devansh <d5nagpal@uwaterloo.ca>
2024-08-16 18:05:56 -0400

    comment

--- Landing_page/geminiapp_website/src/components/contact.jsx
+++ Landing_page/geminiapp_website/src/components/contact.jsx
@@ -3,11 +3,12 @@ import "../styles/contact.css";
 
 const Contact = () => {
   // Capitalize component name
+  //under construction
   return (
     <div style={{ textAlign: "center", padding: "50px" }}>
       <h1 style={{ fontSize: "48px", marginBottom: "20px" }}>Unavailable</h1>
       <p style={{ fontSize: "36px", color: "White" }}>
-        The Contact section is under construction. Check back at a later time!
+        The Contact section is under construction. Check back at a later time! 
       </p>
     </div>
   );
