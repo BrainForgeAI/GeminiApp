cd2d64ddafb533dc90ddc1d5f48b013d3d5e3c5e
khanayaani <113151779+khanayaani@users.noreply.github.com>
2024-08-18 14:46:59 -0400

    finished footer
    need to fix colour scheme, position on About page, update logo

--- Landing_page/geminiapp_website/src/components/footer.jsx
+++ Landing_page/geminiapp_website/src/components/footer.jsx
@@ -1,25 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/footer.css";
+import { IonIcon } from "@ionic/react";
+import { logoGithub } from "ionicons/icons";
+import { logoLinkedin } from "ionicons/icons";
 
 const Footer = () => {
   return (
     <section className="section-footer">
       <div className="footer-left">
-        <span className="brand-name">BRAINFORGE</span>
-        <span className="brand-bio">A smart way to learn</span>
+        <p>
+          <span className="copyright">&copy;2024</span> BRAINFORGE
+        </p>
       </div>
       <div className="footer-mid">
-        <span>Get up to date on the latest updates</span>
-        <span>MAYBE EMAIL BOX HERE</span>
+        <img
+          className="logo-img"
+          src={require("../Assets/final_logo.png")}
+          alt="BrainForge Logo"
+        />
       </div>
       <div className="footer-right">
-        <span>Follow us</span>
-        <a href="https://www.linkedin.com/" target="_blank">
-          LinkedIn
+        <a
+          className="footer-link"
+          href="https://www.linkedin.com/company/brainforge-ai/about/"
+          target="_blank"
+        >
+          <IonIcon icon={logoLinkedin}></IonIcon>
         </a>
-        <a href="https://www.github.com/" target="_blank">
-          Github
+        <a
+          className="footer-link"
+          href="https://github.com/BrainForgeAI"
+          target="_blank"
+        >
+          <IonIcon icon={logoGithub}></IonIcon>
         </a>
       </div>
     </section>
