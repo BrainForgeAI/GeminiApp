a11c20d78a0993c18c032bb665cce7d621431579
khanayaani <113151779+khanayaani@users.noreply.github.com>
2024-09-02 00:21:44 -0400

    finished gallery landing

--- Landing_page/geminiapp_website/src/components/home.jsx
+++ Landing_page/geminiapp_website/src/components/home.jsx
@@ -38,7 +38,7 @@ const Homepage = () => {
             <iframe
               width="560"
               height="315"
-              src="https://www.youtube.com/embed/IBP5NUDP28A?si=FXJjmZIJ63ruJ_F5"
+              src="https://www.youtube.com/embed/AKQTWVEM-Xo?si=Zu95CqAIO-Ji10_Z"
               title="YouTube video player"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
