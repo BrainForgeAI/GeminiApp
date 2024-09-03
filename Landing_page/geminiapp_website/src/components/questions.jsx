d8ce520f82dc811767cd5d7f71caec5ebe226a79
Vikram <vikramchahal24@gmail.com>
2024-09-03 19:41:24 -0400

    Working button when opening faqs

--- Landing_page/geminiapp_website/src/components/questions.jsx
+++ Landing_page/geminiapp_website/src/components/questions.jsx
@@ -5,6 +5,9 @@ function Questions({ faq, index, toggleFAQ }) {
     <div className={"faq " + (faq.open ? 'open' : '')} key={index}>
       <div className="faq-question" onClick={() => toggleFAQ(index)}>
         {faq.question}
+        <span className={"faq-arrow " + (faq.open ? 'open' : '')}>
+          {faq.open ? '▲' : '▼'}
+        </span>
       </div>
       {faq.open && (
         <div className="faq-answer">
